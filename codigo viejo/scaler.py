3e48f138c6bbb2af9e231adda472fcd781604d7d
Federico-Gil <59852188+Federico-Gil@users.noreply.github.com>
2021-06-12 20:59:45 -0300

    Chau Chau adio

--- codigo viejo/scaler.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-basewidth = 28
-img = Image.open('f.png')
-wpercent = (basewidth / float(img.size[0]))
-hsize = int((float(img.size[1]) * float(wpercent)))
-img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-img.save('f_o.png')
--- codigo viejo/size_normalizer.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-def normalize_size(img_name,size):
-    filename = img_name
-    img = Image.open(filename)
-    wpercent = (size / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((size, hsize), Image.ANTIALIAS)
-    img.save(filename)
